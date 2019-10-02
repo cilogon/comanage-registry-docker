7c6f645200ffc2af6380bae4b96c0e3a5815cc10
Scott Koranda <skoranda@gmail.com>
2019-10-02 12:56:08 -0500

    First commit of Dockerfile for nginx as SATOSA proxy
    First commit of Dockerfile and related to build image for nginx as a
    SATOSA proxy. This is necessary to serve static information like an SP
    information and privacy policy URL often required by federations.

---
+++ comanage-registry-nginx-satosa-proxy/start.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+cat>/etc/nginx/nginx.conf<<EOF
+daemon off;
+
+user  nginx;
+worker_processes  1;
+
+error_log /dev/stdout info;
+pid        /var/run/nginx.pid;
+
+events {
+    worker_connections  1024;
+}
+
+
+http {
+    include       /etc/nginx/mime.types;
+    default_type  application/octet-stream;
+
+    log_format  main  '\$remote_addr - \$remote_user [\$time_local] "\$request" '
+                      '\$status \$body_bytes_sent "\$http_referer" '
+                      '"\$http_user_agent" "\$http_x_forwarded_for"';
+
+    access_log /dev/stdout main;
+
+    keepalive_timeout  65;
+
+    server {
+      listen 80;
+
+      location / {
+         root /usr/share/nginx/html;
+         try_files \$uri @satosa;
+      }
+
+      location @satosa {
+         proxy_pass ${SATOSA_BASE_URL};
+      }
+   }
+
+}
+EOF
+
+nginx -c /etc/nginx/nginx.conf
