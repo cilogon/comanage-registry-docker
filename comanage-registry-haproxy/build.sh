4956b9292899a4a8b2272f8e90ce780cf6784183
Terry Fleury <tfleury@illinois.edu>
2022-02-23 06:16:38 -0600

    Update cilogon-haproxy to 2.5.3

---
+++ comanage-registry-haproxy/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [[ -z "${HAPROXY_VERSION}" ]] ; then
+   echo "Please set HAPROXY_VERSION environment variable."
+   echo "E.g., 'export HAPROXY_VERSION=2.5.3"
+   exit;
+fi
+
+docker build --no-cache -t cilogon-haproxy:${HAPROXY_VERSION} .
+
+echo
+echo "Now you can tag and upload the image to AWS ECR:"
+echo "docker tag cilogon-haproxy:${HAPROXY_VERSION} 495649616520.dkr.ecr.us-east-2.amazonaws.com/cilogon-haproxy:${HAPROXY_VERSION}"
+echo "docker push 495649616520.dkr.ecr.us-east-2.amazonaws.com/cilogon-haproxy:${HAPROXY_VERSION}"
