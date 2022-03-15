78db94c30296e7b2b9456198e7e7961256d01dec
Terry Fleury <tfleury@illinois.edu>
2022-03-15 10:07:54 -0500

    CIL-1198 httpd image updated to 2.4.53

---
+++ comanage-registry-getssl/build.sh
@@ -0,0 +1,19 @@
+VERSION=4
+IMAGE=comanage-registry-getssl
+
+docker build --no-cache -t ${IMAGE}:${VERSION} .
+
+echo
+echo "If image built successfully, do the following."
+echo
+echo "First log in to AWS:"
+echo
+echo "    aws ecr get-login-password --region us-east-2 | \\"
+echo "    docker login --username AWS --password-stdin \\"
+echo "           495649616520.dkr.ecr.us-east-2.amazonaws.com"
+echo
+echo "Then tag and push the newly build image to AWS ECR:"
+echo
+echo "    docker tag ${IMAGE}:${VERSION} 495649616520.dkr.ecr.us-east-2.amazonaws.com/${IMAGE}:${VERSION}"
+echo "    docker push 495649616520.dkr.ecr.us-east-2.amazonaws.com/${IMAGE}:${VERSION}"
+
